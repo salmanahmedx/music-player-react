d5127be99ce4bf881fe1eb71b23fd2d9ad3911b1
Salman Ahmed <salmanahmedx@hotmail.com>
2022-07-09 04:38:38 +0600

    Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song></Song>
+      <Player></Player>
     </div>
   );
 }
 
 export default App;
+
+
