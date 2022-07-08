fb2606b23641ed501602d99748e2ea51bc65c500
Salman Ahmed <salmanahmedx@hotmail.com>
2022-07-09 05:29:01 +0600

    state - songs, currentSongs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./style/style.scss";
+import data from "./util";
+
 
 function App() {
+  //data() returns array of song details from util.js
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0])
   return (
     <div className="App">
-      <Song></Song>
+      <Song currentSong={currentSong}></Song>
       <Player></Player>
     </div>
   );
