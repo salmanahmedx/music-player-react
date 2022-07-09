bbd8bcd454c6bf536280378c2f69ec2c4e777ef6
Salman Ahmed <salmanahmedx@hotmail.com>
2022-07-09 08:05:05 +0600

    toggle pause/play button

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons"
+import { faPlay, faAngleLeft, faAngleRight, faPause } from "@fortawesome/free-solid-svg-icons"
 import { useState } from 'react';
 
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
@@ -48,7 +48,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleLeft}></FontAwesomeIcon>
-                <FontAwesomeIcon onClick={playSongHandler} className='play' size='2x' icon={faPlay}></FontAwesomeIcon>
+                <FontAwesomeIcon onClick={playSongHandler} className='play' size='2x' icon={isPlaying ? faPause : faPlay}></FontAwesomeIcon>
                 <FontAwesomeIcon className='skip-forward' size='2x' icon={faAngleRight}></FontAwesomeIcon>
             </div>
             <audio onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentSong.audio} onLoadedMetadata={timeUpdateHandler}></audio>
